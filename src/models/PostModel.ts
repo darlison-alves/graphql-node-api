a3512b2cafdd3c98301f24fc3035209b224e92fb
Darlison <darlinsonalves@gmail.com>
2019-06-26 22:56:59 -0300

    criando demais models..

--- src/models/PostModel.ts
+++ src/models/PostModel.ts
@@ -1,5 +1,6 @@
 import { Instance, Model, Sequelize, DataTypes } from "sequelize";
 import { BaseModelInterface } from "../interfaces/BaseModelInterface";
+import { ModelsInterface } from "../interfaces/ModelsInterface";
 
 export interface PostAttributes {
     id?: number;
@@ -16,7 +17,40 @@ export interface PostInstance extends Instance<PostAttributes>{}
 export interface PostModel extends BaseModelInterface, Model<PostInstance, PostAttributes>{}
 
 export default ( sequilize: Sequelize, DataTypes: DataTypes): PostModel => {
-    const post: PostModel = sequilize.define('Post', {
-        
+    let Post: PostModel = sequilize.define('Post', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        photo: {
+            type: DataTypes.BLOB({
+                length: 'long'
+            }),
+            allowNull: false
+        }
+    },{
+        tableName: 'posts'
     })
+
+    Post.associate = (models: ModelsInterface): void => {
+        Post.belongsTo(models.User, {
+            foreignKey:{
+                allowNull: false,
+                field: 'author',
+                name: 'author'
+            }
+        })
+    };
+
+    return Post
 }
